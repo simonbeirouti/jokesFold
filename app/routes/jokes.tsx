fe1aa2ffe5a6176696dc7cc7a1ed33e7a8103804
simonbeirouti <hello@simonbeirouti.com>
2022-08-14 13:46:20 +1000

    check user and choose less jokes

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -4,22 +4,28 @@ import { Outlet, Link, useLoaderData } from "@remix-run/react";
 
 import stylesUrl from "~/styles/jokes.css";
 import { db } from "~/utils/db.server";
-
+import { getUser } from "~/utils/session.server";
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
 type LoaderData = {
+  user: Awaited<ReturnType<typeof getUser>>;
   jokeListItems: Array<{ id: string; name: string }>;
 };
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({
+  request,
+}) => {
+  const jokeListItems = await db.joke.findMany({
+    take: 5,
+    select: { id: true, name: true },
+    orderBy: { createdAt: "desc" },
+  });
+  const user = await getUser(request);
   const data: LoaderData = {
-    jokeListItems: await db.joke.findMany({
-      take: 5,
-      select: { id: true, name: true },
-      orderBy: { createdAt: "desc" },
-    }),
+    jokeListItems,
+    user,
   };
   return json(data);
 };
@@ -40,6 +46,18 @@ export default function JokesRoute() {
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">
@@ -51,7 +69,6 @@ export default function JokesRoute() {
               {data.jokeListItems.map(j => (
                 <li key={j.id}>
                   <Link to={j.id}>{j.name}</Link>
-                  {/* <Link to={j.name.toLowerCase()}>{j.name}</Link> */}
                 </li>
               ))}
             </ul>
